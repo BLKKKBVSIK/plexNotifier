6c6f3bb20c8b1d9009d77c4957e1c92036ed0715
Enzo CONTY <indefined3@gmail.com>
2021-10-12 22:01:37 +0200

    :recycle: Refactoring the handling of the Plex payload

--- bin/model/plex_payload.dart
+++ bin/model/plex_payload.dart
@@ -5,7 +5,9 @@ class PlexPayload {
   final String? grandparentTitle;
   final String? parentTitle;
   final String? summary;
-  final double audienceRating;
+  final double? audienceRating;
+  final int? parentIndex;
+  final int? index;
 
   const PlexPayload({
     this.event,
@@ -14,55 +16,60 @@ class PlexPayload {
     this.grandparentTitle,
     this.parentTitle,
     this.summary,
-    required this.audienceRating,
+    this.parentIndex = 0,
+    this.index = 0,
+    this.audienceRating,
   });
 
   factory PlexPayload.fromJson(Map<String, dynamic> json) => PlexPayload(
         event: json['event'] as String?,
         title: json['Metadata']['title'] as String?,
-        showType: json['Metadata']['librarySectionType'] as String?,
+        showType: json['Metadata']['librarySectionType'] as String? ?? '',
         grandparentTitle: json['Metadata']['grandparentTitle'] as String?,
         parentTitle: json['Metadata']['parentTitle'] as String?,
         summary: json['Metadata']['summary'] as String?,
         audienceRating: json['Metadata']['audienceRating'] ?? 0.0,
+        parentIndex: (json['Metadata'] as Map<String, dynamic>)
+                .containsKey('parentIndex')
+            ? int.tryParse(json['Metadata']['parentIndex'] ?? 0.0)
+            : 0,
+        index: (json['Metadata'] as Map<String, dynamic>).containsKey('index')
+            ? int.tryParse(json['Metadata']['index'] ?? 0.0)
+            : 0,
       );
 
   @override
-  List<Object?> get props =>
-      [event, title, grandparentTitle, parentTitle, summary, audienceRating];
+  List<Object?> get props => [
+        event,
+        title,
+        grandparentTitle,
+        parentTitle,
+        summary,
+        audienceRating,
+        index,
+        parentIndex
+      ];
 
-  String createJsonForDiscordWebhook() {
-    return '''
-  {
-    "username": "",
-    "avatar_url": "",
-    "content": "",
-    "embeds": [
-      {
-        "title": "There is a new ${this.showType} on the PleX",
-        "color": 0,
-        "description": "${this.summary}",
-        "timestamp": "",
-        "author": {},
-        "image": {},
-        "thumbnail": {},
-        "footer": {
-          "text": "Audience rating: ${this.audienceRating.toString()}/10"
-        },
-        "fields": [
-          {
-            "name": "Title",
-            "value": "${this.title}"
+  String createJsonForDiscordWebhook(String? link) {
+    String query = '''
+    {
+      "embeds": [
+        {
+          "title": "${(showType ?? '').toLowerCase().contains("show") ? "${grandparentTitle ?? 'Unknow content title'} - ${title ?? 'Unknow subcontent title'} (S$parentIndex - E$index)" : "${title ?? 'Unknow content title'} "}",
+          "description": "${summary ?? 'No description available'}",
+          "footer": {
+            "text": "Audience rating: $audienceRating/10"
           },
-          {
-            "name": "Serie Title",
-            "value": "Ooooo"
+          "thumbnail": {
+            "url": "${link ?? 'https://cdn.discordapp.com/embed/avatars/3.png'}"
+          },
+          "author": {
+            "name": "New ${showType ?? 'unknowTypeShow'}"
           }
-        ]
-      }
-    ],
-    "components": []
-  }
-  ''';
+        }
+      ]
+    }
+    ''';
+    return query;
   }
 }
